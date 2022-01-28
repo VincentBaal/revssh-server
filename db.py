2db561eacf21f0e4938dc425c27764d990141255
Vincent Baalmans <vincentbaalmans@gmail.com>
2022-01-28 13:06:03 +0100

    Can now generate api_keys and store in local db.

--- db.py
+++ db.py
@@ -2,14 +2,57 @@ import sqlite3
 
 con = sqlite3.connect('storage.db')
 
-
-def add_new_tunnel():
+# Add a new tunnel entry to the local db
+def add_tunnel(tunnel_id, api_key, port, last_request):
     cur = con.cursor()
-    cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+    query = f"INSERT INTO tunnel VALUES ('{tunnel_id}','{api_key}', {port},{last_request})"
+    cur.execute(query)
+    con.commit()
+    cur.close()
+
+
+# Check db for a row using this port
+def is_port_in_use(port):
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM tunnel WHERE port = ?", (port,))
+    result = cur.fetchone()
+    cur.close()
+    return result is not None
+
+
+# Check db for a row containing the tunnel_id
+def is_id_in_use(tunnel_id):
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM tunnel WHERE tunnel_id = ?", (tunnel_id,))
+    result = cur.fetchone()
+    cur.close()
+    return result is not None
+
+
+# Get the highest port in use by the clients
+def get_last_used_port():
+    cur = con.cursor()
+    cur.execute("SELECT port FROM tunnel ORDER BY port DESC;")
+    port = cur.fetchone()
+    cur.close()
+    print(str(port))
+    if port is None:
+        return 9000
+    return port[0]
 
 
 def init_db():
     cur = con.cursor()
     # Create table
     cur.execute('''CREATE TABLE IF NOT EXISTS tunnel 
-                   (tunnel_id text, api_key text, last_request integer)''')
+                   (tunnel_id text, api_key text, port integer, last_request integer)''')
+    con.commit()
+    cur.close()
+
+
+# Delete all data from the local DB
+def truncate():
+    cur = con.cursor()
+    cur.execute('''DELETE FROM tunnel''')
+    con.commit()
+    cur.close()
--- main.py
+++ main.py
@@ -1,42 +1,66 @@
-from waitress import serve
-from flask import Flask, request
-import os
-import logger
+import secrets
 import sys
-import json
+
+from flask import Flask
+from waitress import serve
+
 import db
+import logger
 
 app = Flask(__name__)
-config = None
-
-
-@app.route("/login", methods=['POST'])
-def login():
-    request_data = request.get_json()
-    if request_data['username'] == config["username"] and request_data['password'] == config["password"]:
-        return "key"
-    else:
-        return "error"
 
 
 @app.route("/getServerKey", methods=['GET'])
 def get_server_key():
-    return "serverkey"
+    return ""
 
 
-
-
-
-def load_config():
-    if not os.path.exists("./config.json"):
-        logger.error("Could not find config.json file. Exiting")
+# Generate an api key for use in the client software with a recognizable id.
+# Optionally provide the port the client should use.
+# If no port is provided the next available port above 9000 will be used.
+def generate_api_key(tunnel_id, port=None):
+    if db.is_id_in_use(tunnel_id):
+        logger.error(f"Id {tunnel_id} is already in use. Please use another id to create a new api key")
         sys.exit()
-    global config
-    config = json.loads(open("./config.json").read())
+
+    api_key = secrets.token_urlsafe(36)
+    if port is None:
+        port = (db.get_last_used_port() + 1)
+    if db.is_port_in_use(port):
+        logger.error(f"Port {port} is already in use. Please enter another port to create a new api key")
+        sys.exit()
+    db.add_tunnel(tunnel_id, api_key, port, 0)
+    logger.info(f"Created new tunnel entry with id {tunnel_id} which will become avaiable on port {port}"
+                f"\n api key: {api_key}")
+
+
+def print_help():
+    logger.info("\nGenerate:\nUse the command -generate with a desired identifier to generate a key for use in the "
+                "tunneling software.\n"
+                "The key will be stored locally on this machine and can be used to authorize requests between "
+                "the tunnel client and server."
+                "\nFormat: \"-generate TUNNEL_ID [optionally PORT]\"\n\n"
+                "Server:\nRun the application without any arguments to start the server.")
+
+
+def handle_command():
+    if len(sys.argv) > 2 and sys.argv[1] == "-generate":
+        if len(sys.argv) > 3:
+            generate_api_key(sys.argv[2], int(sys.argv[3]))
+        else:
+            generate_api_key(sys.argv[2])
+    elif sys.argv[1] == "-help":
+        print_help()
+    elif sys.argv[1] == "-truncate":
+        db.truncate()
+    else:
+        logger.error("Wrong command line argument. Use -help for more information")
 
 
 if __name__ == '__main__':
-    logger.info("Starting revssh-server")
     db.init_db()
-    load_config()
-    serve(app, listen='*:8989')
+    if len(sys.argv) > 0:
+        handle_command()
+    else:
+        logger.info("Starting revssh-server")
+        serve(app, listen='*:8989')
